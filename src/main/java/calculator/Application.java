cd95f19d5ee24c70304ec55e3c1e54c9f52b58ad
hyeonn9 <1319ono@gmail.com>
2025-10-19 19:37:04 +0900

    feat: add isCustom

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = sc.nextLine();
+
+        Calculator cal = new Calculator();
+
+        String[] inputSplit = new String[input.length()];
+
+        if(cal.isCustom(input)){
+
+        } else{
+
+        }
+
+    }
+
+    public static class Calculator {
+        // 커스텀인지 아닌지 구별
+        public boolean isCustom(String input){
+            if(input.startsWith("//")){
+                return true;
+            }
+            return false;
+        }
+
     }
 }
